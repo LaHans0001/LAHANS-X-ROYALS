8f21ecf7caab7d8df781abd48d199c9c6e80c10f
LaHans0001 <77910759+LaHans0001@users.noreply.github.com>
2021-01-24 10:30:21 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"600cfd6fae0c05001e87a84e",
+				"600cfd6fae0c05001e87a850"
+			]
+		},
+		"600cfd6fae0c05001e87a84e": {
+			"id": "600cfd6fae0c05001e87a84e",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"600cfd6fae0c05001e87a850": {
+			"id": "600cfd6fae0c05001e87a850",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
